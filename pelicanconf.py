b9e9b01f9f5dbb1c458f6537f9ca12387ea34853
Nikolay Koldunov <koldunovn@gmail.com>
2013-09-23 23:17:36 +0200

    switch to the new ipython notebook plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,8 +8,14 @@ SITEURL = 'http://earthpy.org'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
-PLUGINS = ['pelican.plugins.ipythonnb']
-MARKUP = ('md', 'ipynb')
+PLUGIN_PATH = '/home/magik/INTERNET/EARTHPY/pelican-plugins'
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+
+#NOTEBOOK_DIR = '/home/magik/INTERNET/EARTHPY/earthpy.org/content2/notebooks'
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+MARKUP = ('md')
 # Blogroll
 LINKS =  (('Python for the Atmospheric and Oceanic Sciences', 'http://pyaos.johnny-lin.com/'),
           ('koldunov.net (in Russian)', 'http://koldunov.net'),
@@ -20,7 +26,7 @@ SOCIAL = ((' ', '#'),
          )
 
 DEFAULT_PAGINATION = False
-TYPOGRIFY = True
+#TYPOGRIFY = True
 SUMMARY_MAX_LENGTH = 100
 DISQUS_SITENAME = 'earthpy'
 GOOGLE_ANALYTICS = 'UA-40120864-1'
